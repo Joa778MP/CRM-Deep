1f099526213c4e287bb99c91e4fad4b5c610294b
Joa778MP <joa.aistuff@gmail.com>
2025-10-03 13:55:36 -0600

    Update globalConfig.js

--- app/javascript/shared/store/globalConfig.js
+++ app/javascript/shared/store/globalConfig.js
@@ -51,8 +51,8 @@ const state = {
 export const getters = {
   get: $state => $state,
   isOnChatwootCloud: $state => $state.deploymentEnv === 'cloud',
-  isACustomBrandedInstance: $state => $state.installationName !== 'Chatwoot',
-  isAChatwootInstance: $state => $state.installationName === 'Chatwoot',
+  isACustomBrandedInstance: $state => $state.installationName !== 'DAIC CRM',
+  isAChatwootInstance: $state => $state.installationName === 'DAIC CRM',
 };
 
 export const actions = {};
