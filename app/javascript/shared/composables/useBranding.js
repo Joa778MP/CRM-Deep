30cd92c723093306fb6a136f87570c11ea248736
Joa778MP <joa.aistuff@gmail.com>
2025-10-03 13:55:15 -0600

    Update useBranding.js

--- app/javascript/shared/composables/useBranding.js
+++ app/javascript/shared/composables/useBranding.js
@@ -17,7 +17,7 @@ export function useBranding() {
     const installationName = globalConfig.value?.installationName;
     if (!installationName) return text;
 
-    return text.replace(/DAIC CRM/g, installationName);
+    return text.replace(/Chatwoot/g, installationName);
   };
 
   return {
